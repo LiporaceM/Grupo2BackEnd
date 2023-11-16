b2791adc6d68d81564203208155893c299c74c96
Matheus Liporace <matheusliporace@gmail.com>
2023-11-16 08:31:35 -0300

    consertando semana 1

--- v0/semana1.py
+++ v0/semana1.py
@@ -1,6 +1,7 @@
 import pydicom
-ds = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_CT_1h")
+ds = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_CT_1h.dcm")
 ds.PatientName = "Matheus"
 ds.PatientBirthDate= "01011996"
-ds.save_as("NovoCT_1h.dcm",ds)
+ds.save_as("Novo_CT_1h.dcm",ds)
+print(ds)
 print("'"*20)
