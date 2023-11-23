e33bdceae7220771cbe6b4243d29b40bfdbd20a2
LiporaceM <108703224+LiporaceM@users.noreply.github.com>
2023-11-23 00:05:32 -0300

    consertando v0 e v1

--- v0/semana1.py
+++ v0/semana1.py
@@ -1,7 +1,8 @@
 import pydicom
-ds = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_CT_1h.dcm")
+ds = pydicom.dcmread("C:/Users/Matheus/Desktop/ibmec/Grupo2BackEnd/Novo_CT_1h.dcm")
+print(ds)
+print("-"*20)
 ds.PatientName = "Matheus"
 ds.PatientBirthDate= "01011996"
 ds.save_as("Novo_CT_1h.dcm",ds)
 print(ds)
-print("'"*20)
--- v1/v1.0.py
+++ v1/v1.0.py
@@ -1,18 +1,31 @@
 import pydicom
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import filters, measure, morphology
 
 # Carregar a Imagem
-ds = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_CT_1h")
-imagem = ds.pixel_array
+ds = pydicom.dcmread("C:/Users/Matheus/Desktop/ibmec/Grupo2BackEnd/Novo_CT_1h.dcm", force=True)
 
-# Visualizar uma parte da imagem original
-plt.imshow(imagem[0], cmap='gray')
-plt.title("Primeira fatia")
-plt.show()
+# Check if 'PixelData' is present in the DICOM file
+if 'PixelData' in ds:
+    pixel_data = ds.PixelData
+    # Get image dimensions from DICOM metadata
+    rows = ds.Rows
+    columns = ds.Columns
 
-# Visualizar outra parte da imagem original
-plt.imshow(imagem[imagem.shape[0] // 2], cmap='gray')
-plt.title("Segunda fatia")
-plt.show()
+    # Determine the datatype based on the BitsAllocated attribute
+    dtype = np.int16 if ds.BitsAllocated == 16 else np.uint8
+
+    # Convert the pixel data to a NumPy array
+    imagem = np.frombuffer(pixel_data, dtype=dtype).reshape((rows, columns))
+    
+    # Visualizar a imagem original
+    plt.imshow(imagem, cmap='gray')
+    plt.title("Imagem Original")
+    plt.show()
+
+    # Visualizar outra parte da imagem original
+    plt.imshow(imagem[imagem.shape[0] // 2], cmap='gray')
+    plt.title("Segunda fatia")
+    plt.show()
+else:
+    print("PixelData attribute not found in the DICOM file.")
