60ae9c01e9ccbc092084499e81c31792fdb67717
LiporaceM <108703224+LiporaceM@users.noreply.github.com>
2023-11-23 00:53:37 -0300

    v2

--- v2/seg.py
+++ v2/seg.py
@@ -2,77 +2,53 @@ import pydicom
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from skimage import filters, measure, morphology
 
-# Carregar a Imagem
-ds = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_CT_1h")
-imagem = ds.pixel_array
+# Load the DICOM file
+dicom_file_path = "C:/Users/Matheus/Desktop/ibmec/Grupo2BackEnd/Novo_CT_1h.dcm"
+ds = pydicom.dcmread(dicom_file_path, force=True)
 
-# Visualizar uma parte da imagem original
-plt.imshow(imagem[0], cmap='gray')
-plt.title("Primeira fatia")
-plt.show()
+# Check if 'PixelData' is present in the DICOM file
+if 'PixelData' in ds:
+    # Extract pixel data and image dimensions
+    pixel_data = ds.PixelData
+    rows = ds.Rows
+    columns = ds.Columns
 
-# Visualizar outra parte da imagem original
-plt.imshow(imagem[imagem.shape[0] // 2], cmap='gray')
-plt.title("Segunda fatia")
-plt.show()
+    # Convert pixel data to NumPy array
+    dtype = np.int16 if ds.BitsAllocated == 16 else np.uint8
+    imagem = np.frombuffer(pixel_data, dtype=dtype).reshape((rows, columns))
 
-# Aplicar um filtro de média slice por slice
-imagem_filtrada = np.array([cv2.blur(slice, (5, 5)) for slice in imagem])
+    # Display the original DICOM image
+    plt.imshow(imagem, cmap='gray')
+    plt.title("Imagem Original")
+    plt.show()
 
-# Visualizar uma fatia da imagem filtrada
-plt.figure()
-plt.imshow(imagem_filtrada[imagem_filtrada.shape[0] // 2], cmap='gray')
-plt.title('Imagem Filtrada')
-plt.show()
+    # Normalize pixel values to the range [0, 255]
+    normalized_imagem = ((imagem - np.min(imagem)) / (np.max(imagem) - np.min(imagem)) * 255).astype(np.uint8)
 
-# Limiarização simples slice por slice
-_, imagem_limiar = cv2.threshold(imagem_filtrada[imagem_filtrada.shape[0] // 2], 50, 255, cv2.THRESH_BINARY)
+    # Apply a mean filter to the image
+    filtered_image = cv2.blur(normalized_imagem, (5, 5))
 
-plt.figure()
-plt.imshow(imagem_limiar, cmap='gray')
-plt.title('Imagem Limiarizada')
-plt.show()
+    # Display the filtered image
+    plt.imshow(filtered_image, cmap='gray')
+    plt.title('Imagem Filtrada')
+    plt.show()
 
-# Dilatação slice por slice
+    # Apply a Gaussian filter to the image
+    blurred_image = cv2.GaussianBlur(normalized_imagem, (5, 5), 0)
 
-kernel = np.ones((5, 5), np.uint8)
-imagem_dilatada = np.array([cv2.dilate(slice, kernel) for slice in imagem_limiar])
+    # Display the Gaussian filtered image
+    plt.imshow(blurred_image, cmap='gray')
+    plt.title('Filtro Gaussian')
+    plt.show()
 
-plt.figure()
-plt.imshow(imagem_dilatada[imagem_dilatada.shape[0] // 2], cmap='gray')
-plt.title('Imagem Dilatada')
-plt.show()
+    # Apply a median filter to the image
+    median_blurred_image = cv2.medianBlur(normalized_imagem, 5)
 
-# Rotulagem de componentes conectados
-imagem_dilatada_8bits = cv2.convertScaleAbs(imagem_dilatada)
-_, labels = cv2.connectedComponents(imagem_dilatada_8bits)
+    # Display the median filtered image
+    plt.imshow(median_blurred_image, cmap='gray')
+    plt.title('Filtro Median')
+    plt.show()
 
-plt.figure()
-plt.imshow(labels, cmap='nipy_spectral')
-plt.title('Imagem Rotulada')
-plt.show()
-
-## Modificando a imagem no DICOM
-ds.PixelData = labels.astype(np.uint8).tobytes()
-
-# Atualizando as tags DICOM para garantir consistência
-ds.Rows, ds.Columns = labels.shape
-ds.SamplesPerPixel = 1
-ds.BitsStored = 8
-ds.BitsAllocated = 8
-ds.HighBit = 7
-
-# Salvando como um novo arquivo DICOM
-ds.save_as("C:/Users/202302417949/Grupo2BackEnd/Novo_Att_CT_1h")
-
-# 4. Leitura do arquivo DICOM modificado
-ds_modificado = pydicom.dcmread("C:/Users/202302417949/Grupo2BackEnd/Novo_Att_CT_1h")
-imagem_reaberta = ds_modificado.pixel_array
-
-# 5. Visualização da imagem reaberta
-plt.figure()
-plt.imshow(imagem_reaberta[0, :, :], cmap='gray')  # assumindo que 'imagem_reaberta' é uma matriz 3D
-plt.title('Imagem Modificada Reaberta')
-plt.show()
+else:
+    print("PixelData attribute not found in the DICOM file.")
