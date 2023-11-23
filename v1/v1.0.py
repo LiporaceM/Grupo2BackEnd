b17fce10328ace0dfe2a41cf386ad30c2b4eb53a
LiporaceM <108703224+LiporaceM@users.noreply.github.com>
2023-11-23 00:13:26 -0300

    v1

--- v1/v1.0.py
+++ v1/v1.0.py
@@ -2,28 +2,27 @@ import pydicom
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Carregar a Imagem
+
 ds = pydicom.dcmread("C:/Users/Matheus/Desktop/ibmec/Grupo2BackEnd/Novo_CT_1h.dcm", force=True)
 
-# Check if 'PixelData' is present in the DICOM file
+
 if 'PixelData' in ds:
     pixel_data = ds.PixelData
-    # Get image dimensions from DICOM metadata
+
     rows = ds.Rows
     columns = ds.Columns
 
-    # Determine the datatype based on the BitsAllocated attribute
+
     dtype = np.int16 if ds.BitsAllocated == 16 else np.uint8
 
-    # Convert the pixel data to a NumPy array
+
     imagem = np.frombuffer(pixel_data, dtype=dtype).reshape((rows, columns))
-    
-    # Visualizar a imagem original
+
     plt.imshow(imagem, cmap='gray')
     plt.title("Imagem Original")
     plt.show()
 
-    # Visualizar outra parte da imagem original
+ 
     plt.imshow(imagem[imagem.shape[0] // 2], cmap='gray')
     plt.title("Segunda fatia")
     plt.show()
